f9731e5e87ce784bd90cd94b4be75b22b213d54f
kerans09 <angel.p.lopez@outlook.com>
2024-02-28 01:37:42 -0600

    feat(): This commit changes eliminates traceCOnfig and plotConfig classes and gets them replaced with configuration dictionaries which are private within `plot` and `trace` classes Default configuration dictionaries are defined in order to get them initialized
    fix(): This commit puts the initialization of `plot` and `trace`
    class' attributes within __init__() method because if they are defined
    and initialized at class level, they will be shared among al class instances
    which is inadequate for our application
    
    X and Y data of the traces are type annotated as lists of floats
    instead of lists of tuples as there was no reason for that definition
    
    get_X_config() method was defined for both trace and plot classes to be
    able to acces the private attribute __meta that holds config data

--- plot.py
+++ plot.py
@@ -1,9 +1,48 @@
-from tools.configurations.config import *
+from tools.configurations.config import Marker, Axis
+from xml.etree.ElementTree import Element
+
+DEFAULT_TRACE_CONFIG:dict = {
+    "name":"header name",
+    "over":"header name",
+    "type":"line",
+    "style":"solid",
+    "color": "#000000",
+    "marker": Marker.CIRCLE,
+    "width": 1,
+    "legend": "generic legend",
+    "reference": [Axis.XMAIN, Axis.YMAIN],
+}
+
+DEFAULT_PLOT_CONFIG:dict = {
+    "file":"global",
+    "title":"plot",
+    "xAxis":{
+            "label": "X Axis",
+            "range":[None, None],
+            "aux":
+                {
+                    "active": False,
+                    "label": "X Axis 2",
+                    "range":[None, None],
+                }
+            },
+    "yAxis":{
+            "label": "Y Axis",
+            "range":[None, None],
+            "aux":
+                {
+                    "active": False,
+                    "label": "Y Axis 2",
+                    "range":[None, None],
+                }
+            }
+}
 
 class trace:
-    Xdata:list[tuple[float]] = None
-    Ydata:list[tuple[float]] = None
-    meta:traceConfig = traceConfig()
+    def __init__(self):
+        self.Xdata:list[float] = None
+        self.Ydata:list[float] = None
+        self.__meta:dict = dict.copy(DEFAULT_TRACE_CONFIG)
 
     def pair_data(self):
         """
@@ -11,16 +50,14 @@ class trace:
         """
 
         if(self.Xdata.__len__() < self.Ydata.__len__()):
-            tuple_size = len(self.Xdata[0])
             while len(self.Xdata) < len(self.Ydata):
-                self.Xdata.append(tuple([0 for _ in range(0,tuple_size)]))
+                self.Xdata.append(0)
         else:
-            tuple_size = len(self.Ydata[0])
             while len(self.Xdata) < len(self.Ydata):
-                self.Ydata.append(tuple([0 for _ in range(0,tuple_size)]))
+                self.Ydata.append(0)
 
 
-    def set_X_data(self,xData:list[tuple[float]])->None:
+    def set_X_data(self,xData:list[float])->None:
         if xData.__len__() < 1:
             self.Xdata = None
         self.Xdata = xData
@@ -29,7 +66,7 @@ class trace:
         if self.Ydata != None:
             self.pair_data()
 
-    def set_Y_data(self,yData:list[tuple[float]])->None:
+    def set_Y_data(self,yData:list[float])->None:
         if yData.__len__() < 1:
             self.Xdata = None
         self.Ydata = yData
@@ -37,39 +74,53 @@ class trace:
         #if X data was already set 
         if self.Xdata != None:
             self.pair_data()
-
+    
     def set_trace_config(self,**kwargs)->None:
         """
         Set the configuration of the trace to be ploted
         """
         for key, value in kwargs.items():
-            if hasattr(self.meta, key):
-                self.meta.__setattr__(key, value)
+            if key in self.__meta.keys():
+                self.__meta[key] = value
             else:
-                print(f"attribute {key} not found in traceConfig")
+                print(f"element {key} not found in trace.__meta")
+    
+    def get_trace_config(self)->dict:
+        """
+        Get the configuration of the trace to be ploted
+        """
+        return self.__meta
+
+
             
 class plot:
-    meta:plotConfig = plotConfig()
-    traces:list[trace] = None
     
     def __init__(self) -> None:
-        pass
+        self.__meta:dict = dict.copy(DEFAULT_PLOT_CONFIG)
+        self.traces:list[trace] = None
 
-    def add_trace(self, xData:list[tuple[float]], yData:list[tuple[float]], **kwargs) ->None:
+    def add_trace(self,**kwargs) ->None:
         new_trace:trace = trace()
-        new_trace.set_trace_config(kwargs=kwargs)
-        new_trace.set_X_data(xData=xData)
-        new_trace.set_Y_data(yData=yData)
+        new_trace.set_trace_config(**kwargs)
+        if self.traces == None:
+            self.traces = []
+        self.traces.append(new_trace)
 
     def clear_pĺot(self) -> None:
-        self.traces.clear()
-    
+        self.traces = []
+
     def set_plot_config(self, **kwargs)->None:
         """
         Set the configuration of the trace to be ploted
         """
         for key, value in kwargs.items():
-            if hasattr(self.meta, key):
-                self.meta.__setattr__(key, value)
+            if key in self.__meta.keys():
+                self.__meta[key] = value
             else:
-                print(f"attribute {key} not found in traceConfig")
+                print(f"element {key} not found in plot.__meta")
+
+    def get_plot_config(self)->dict:
+        """
+        Get the configuration of the trace to be ploted
+        """
+        return self.__meta
